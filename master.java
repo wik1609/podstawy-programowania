788b61ac9b9c06cae0cf4148dc9586e0900feb36
Wiktor Krawczak <wiko3034gmail.com>
2024-03-06 20:59:20 +0100

    chore: init project

---
+++ master.java
@@ -0,0 +1,7 @@
+public class master{
+    public static void main(String[] args) {
+       String imię = "Wiktor";
+       int wiek = 25;
+    System.out.println("Hello " + imię + "!" + " Masz " + wiek + " lat.");
+    }
+}
