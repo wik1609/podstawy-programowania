929c8d0f46c897e7a51c35f105d677b5ed6a6282
Wiktor Krawczak <wiko3034gmail.com>
2024-03-10 17:10:02 +0100

    zmiana

--- zadanie1.java
+++
@@ -1,9 +0,0 @@
-public class zadanie1 {
-
-    public static void main(String[] args) {
-        int a = 7;
-        int b = 7;
-        int sum = a + b;
-    System.err.println("Suma: " + sum);
-    }
-}
